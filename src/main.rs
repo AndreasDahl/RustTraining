fe24dd368e788ae4cb3b3805eb22fa0c229a9d76
Andreas Dahl <andreas@hrdahl.dk>
2015-02-14 17:54:35 +0100

    a little more

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = [Point { x: 0, y: 0 },
+                Point { x: 1, y: 1 }];
+
+    for point in data.iter() {
+        println!("The value of x is: ({}, {})", point.x, point.y);
+    }
 }
