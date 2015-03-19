dab949610da98be73f2e6278f181906290fc1bc8
Andreas Dahl <andreas@hrdahl.dk>
2015-03-19 23:21:24 +0100

    Adapted clustering for FastaString

--- genome/src/clustering.rs
+++ genome/src/clustering.rs
@@ -3,20 +3,29 @@ use std::collections::LinkedList;
 use std::num::Float;
 
 use kmer::kmer;
+use fasta_string::FastaString;
 
-pub fn seq_clust(input: &mut Iterator<Item = &str>, similarity: f32, output: &mut Write) {
-    let mut seeds = LinkedList::new();
+pub fn seq_clust(input: &mut Iterator<Item = FastaString>, similarity: f32, output: &mut Write) {
+    let mut seeds : LinkedList<FastaString> = LinkedList::new();
     let mut cluster_count = 0;
     for current in input {
-        let size = current.len();
+        let size = current.sequence.len();
         // Generate kmer
-        let current_kmer = kmer::generate_three_mer(current);
+        let current_kmer = kmer::generate_three_mer(&current.sequence);
 
         let mut best_dist = Float::infinity();
         let mut n_from_last = 0;
-        for seed in &seeds {
-            best_dist = kmer::hellinger_distance(&current_kmer, seed);
+        for seed in seeds.iter_mut() {
+            let seed_kmer = kmer::generate_three_mer(&seed.sequence);
+            best_dist = kmer::hellinger_distance(&current_kmer, &seed_kmer);
             if best_dist < similarity {
+                writeln!(output,
+                         "H\t{cluster}\t{size}\t{id}\t+\t0\t0\t???\t{query}\t{target}", 
+                         cluster = cluster_count - n_from_last, 
+                         size = size,
+                         id = best_dist,
+                         query = &current.description,
+                         target = seed.description);
                 break;
             }
             n_from_last += 1;
@@ -26,16 +35,10 @@ pub fn seq_clust(input: &mut Iterator<Item = &str>, similarity: f32, output: &mu
             if seeds.len() >= 32 {
                 seeds.pop_back();
             }
-            seeds.push_front(current_kmer);
-            writeln!(output, "S\t{}\t{}\t*\t*\t*\t*\t*\t{}\t*", cluster_count, size, "TEST");
+            writeln!(output, "S\t{}\t{}\t*\t*\t*\t*\t*\t{}\t*", cluster_count, size,
+                     current.description);
+            seeds.push_front(current);
         } else {
-            writeln!(output,
-                     "H\t{cluster}\t{size}\t{id}\t+\t0\t0\t???\t{query}\t{target}", 
-                     cluster = cluster_count - n_from_last, 
-                     size = size,
-                     id = best_dist,
-                     query = "TEST",
-                     target = "TEST");
         }
     }
 }
@@ -45,17 +48,18 @@ mod tests {
     extern crate test;
     use self::test::Bencher;
     use std::io;
+    use std::fs;
 
+    use fasta::FastaIO;
     use super::seq_clust;
 	
     #[test]
     fn test_seq_clust() {
-        let data = vec!["abc", "abc", "abd", "aabc", "cbd", "abc"];
-        //let mut out = io::Cursor::new(Vec::new());
-        
-        seq_clust(&mut data.into_iter(), 0.5, &mut io::stdout());
-        
-        //let s = String::from_utf8(out.into_inner()).ok().expect("ERROR");
+        let mut fasta = FastaIO::new(fs::File::open("res/p3_clean_C-148-2-Caecum_S128_sorted.fa").unwrap());
 
+        seq_clust(&mut fasta, 0.5, &mut io::stdout());
     }
+
+    // Benches ----
+
 }
