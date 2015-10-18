fd6dee077cd27ebbe8ab445ac17fa9c240ce66cd
Andreas Dahl <andreas@hrdahl.dk>
2015-10-18 11:45:20 +0200

    use iterator instead of LinkedList

--- prod_cons/src/lib.rs
+++ prod_cons/src/lib.rs
@@ -1,5 +1,4 @@
 use std::sync::Arc;
-use std::collections::linked_list::LinkedList;
 use std::thread;
 use std::sync::RwLock;
 use std::sync::mpsc;
@@ -8,12 +7,12 @@ use std::fmt::Display;
 pub trait Producer<T : Send + Sync + 'static, G: Send + 'static> {
     fn produce(T) -> G;
 
-    fn start_producing(todo : Arc<RwLock<LinkedList<T>>>, sender : mpsc::Sender<G>)
+    fn start_producing<I : Iterator<Item = T> + Send + Sync + 'static>(todo : Arc<RwLock<I>>, sender : mpsc::Sender<G>)
             -> () {
         Self::start_producers(1, todo, sender);
     }
 
-    fn start_producers(n : u32, todo : Arc<RwLock<LinkedList<T>>>, sender : mpsc::Sender<G>)
+    fn start_producers<I : Iterator<Item = T> + Send + Sync + 'static>(n : u32, todo : Arc<RwLock<I>>, sender : mpsc::Sender<G>)
             -> () {
         for x in 0..n {
             let name = format!("Producer {}", x);
@@ -21,13 +20,12 @@ pub trait Producer<T : Send + Sync + 'static, G: Send + 'static> {
             let todo = todo.clone();
             let sender = sender.clone();
             thread::Builder::new().name(name).spawn(move || {
-                while !todo.read().unwrap().is_empty() {
-                    let mut list = todo.write().unwrap();
-                    let answer = Self::produce(list.pop_front().unwrap());
+                while let Some(next) = todo.write().unwrap().next() {
+                    let answer = Self::produce(next);
                     sender.send(answer).unwrap();
                 }
                 println!("Producer done");
-            }).unwrap();  // TODO: Return result
+            }).unwrap();  // TODO: Return handle
         }
 
     }
@@ -51,6 +49,7 @@ mod tests {
     use std::collections::linked_list::LinkedList;
     use std::sync::Arc;
     use std::sync::mpsc;
+    use std::iter::IntoIterator;
 
     #[test]
     fn it_works() {
@@ -62,7 +61,9 @@ mod tests {
         work.push_back(3);
         work.push_back(4);
 
-        let data = Arc::new(RwLock::new(work));
+        let work_iter = work.into_iter();
+
+        let data = Arc::new(RwLock::new(work_iter));
 
         let (tx, rx) = mpsc::channel();
 
