3b9889afb4e628b072f9c734be3796a647b0be16
Andreas Dahl <andreas@hrdahl.dk>
2017-02-13 11:01:30 +0100

    suffix array benchmarking

--- search/src/lib.rs
+++ search/src/lib.rs
@@ -1,6 +1,7 @@
+#![feature(test)]
+extern crate test;
 
-
-pub fn search(ref_string :&str,
+fn suffix_findall(ref_string :&str,
               a : &[usize],
               pattern : &str) -> [usize; 2] {
     let mut l = 0;
@@ -26,21 +27,27 @@ pub fn search(ref_string :&str,
     return [s, r];
 }
 
-pub fn find_substring(full : &str, pattern : &str) -> Option<usize> {
-    full.find(pattern)
+pub fn search<'a>(ref_string :&'a str,
+              a : &[usize],
+              pattern : &str) -> Vec<&'a str> {
+    let lr = suffix_findall(ref_string, a, pattern);
+    let l = lr[0];
+    let r = lr[1];
+    let len = pattern.len();
+    let mut res = Vec::new();
+    for i in l..r {
+        res.push(&ref_string[a[i]..a[i] + len])
+    }
+    return res;
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::test::Bencher;
 
     #[test]
-    fn test_find_substring() {
-        let res = find_substring("abc", "bc");
-        assert_eq!(res, Some(1));
-    }
-
-    // #[test]
     fn test_suffix_array_hest() {
         // 0"hest "
         // 1"est "
@@ -56,16 +63,43 @@ mod tests {
         let text = "hest ";
         let suffix_array = [4,1,0,2,3];
 
-        assert_eq!(search(text, &suffix_array, "e"), [1,1]);
+        assert_eq!(suffix_findall(text, &suffix_array, "e"), [1,2]);
     }
 
     #[test]
     fn test_suffix_array_banana() {
         let text = "banana ";
         let suffix_array = [6,5,3,1,0,4,2];
-        assert_eq!(search(text, &suffix_array, "ba"), [4,4]);
-        assert_eq!(search(text, &suffix_array, "na"), [5,7]);
-        assert_eq!(search(text, &suffix_array, "a"), [1,4]);
+        assert_eq!(suffix_findall(text, &suffix_array, "ba"), [4,5]);
+        assert_eq!(suffix_findall(text, &suffix_array, "na"), [5,7]);
+        assert_eq!(suffix_findall(text, &suffix_array, "a"), [1,4]);
     }
 
+    fn builtin<'a>(text : &'a str, p : &str) -> Vec<&'a str> {
+        text.matches(p).collect()
+    }
+
+    #[test]
+    fn test_something() {
+        let text = "banana ";
+        let suffix_array = [6,5,3,1,0,4,2];
+        let p = "a";
+        assert_eq!(builtin(text, p), search(text, &suffix_array, p));
+    }
+
+    #[bench]
+    fn bench_suffix(b: &mut Bencher) {
+        let text = "bananaananananananananananananan ";
+        let suffix_array = [6,5,3,1,0,4,2];
+
+        b.iter(|| search(text, &suffix_array, "a"));
+    }
+
+
+    #[bench]
+    fn bench_builtin(b: &mut Bencher) {
+        let text = "bananaananananananananananananan ";
+
+        b.iter(|| builtin(text, "a"));
+    }
 }
